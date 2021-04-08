3f20d7be3ec3441d9b834dcca1a7a49f3973fae8
Marco Herrn <marco@mherrn.de>
2021-04-08 22:18:57 +0200

    Support Ctrl-a for finishing search
    This basically does the same as Ctrl-e, but after that puts the cursor
    at the beginning of the line (which is the normal readline behavior of
    Ctrl-a).
    
    This is done by writing the corresponding readline function into a
    temporary file. Therefore it is possible to apply other readline
    movement functions as well. Since a lot of those movement functions only
    make sense if the cursor is at a specific position _inside_ the
    commandline (e.g. M-b, M-f, C-b, C-f, etc.) a temporary file is prepared
    for preparing the cursor position to put the cursor on before applying
    the corresponding readline function.

--- re_search.fish
+++ re_search.fish
@@ -1,5 +1,7 @@
 function re_search
 	set -x fish_history_file (mktemp -t fish.hist.XXXXXX)
+	set -x fish_cursor_pos_file (mktemp -t fish.curs.XXXXXX)
+	set -x fish_readline_cmd_file (mktemp -t fish.rdln.XXXXXX)
   history --null --reverse -n (echo "1024 * 256" | bc) > "$fish_history_file"
 	set -l tmp (mktemp -t fish.XXXXXX)
 	set -x SEARCH_BUFFER (commandline -b)
@@ -8,6 +10,12 @@ function re_search
 	commandline -f repaint
 	if [ -s $tmp ]
 		commandline -r (cat $tmp)
+	end
+	if [ -s $fish_cursor_pos_file ]
+		commandline -C (cat $fish_cursor_pos_file)
+	end
+	if [ -s $fish_readline_cmd_file ]
+		commandline -f (cat $fish_readline_cmd_file)
 	end
 	if [ $res = 0 ]
 		commandline -f execute
