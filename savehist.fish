eafa5fb9f47c426fbf5fcbef43d9f22ea14a7e85
Marco Herrn <marco@mherrn.de>
2022-01-17 11:08:27 +0100

    Use 'echo' instead of 'printf' for saving history
    echo allows writing escape sequences like '\n' as is. printf would
    replace them with a real newline. Therefore changing the actual
    commandline, which is not desired.

--- savehist.fish
+++ savehist.fish
@@ -1,10 +1,10 @@
-function savehist --on-event fish_postexec
+function savehist --on-event fish_preexec
 	set -x fish_pid_history_file /tmp/my_fish_history_$fish_pid
 	if not [ -f $fish_pid_history_file ]
 		history --null --reverse > $fish_pid_history_file
 	end
 
 	if not [ -z $argv ]
-		printf $argv'\0' >> $fish_pid_history_file
+		echo -n -E $argv'\0' >> $fish_pid_history_file
 	end
 end
