44e7787076b790ccbf91a0ce6fdc647a2d37ebb2
Marco Herrn <marco@mherrn.de>
2025-04-06 22:07:50 +0200

    Combine bash functions in one file
    While fish enforces one function per file this is not necessary in bash
    and only complicated things. Having them all in one script that can be
    sourced at once is preferrable here.

--- bash_functions/re_search.sh
+++ bash_functions/re_search.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
+re_search_savehist() {
+  bash_pid_history_file=/tmp/my_bash_history_$$
+  ## Delete the history file on shell exit
+  trap "rm -rf ${bash_history_file}" EXIT
+
+  ## Save the whole history to our local history file if not yet done
+  if [ ! -f "$bash_pid_history_file" ]; then
+    history | sed 's/^[[:space:]]*[0-9]\+[[:space:]]\+\(.*\)/\1/g' > $bash_pid_history_file
+  fi
+
+  ## Append each executed command to the local history file
+  echo $(history 1 | sed "s/^[[:space:]]*[0-9]\+[[:space:]]\+\(.*\)/\1/g") >> $bash_pid_history_file
+}
+## save history every time a prompt is renewed (== after each command execution)
+export PROMPT_COMMAND='if [ "$(id -u)" -ne 0 ]; then re_search_savehist; fi'
+
 re_search() {
   export bash_history_file="/tmp/re-search_bash_history_${USER}_${BASHPID}"
   if [ ! -e "$bash_history_file" ]; then
-    savehist
+    re_search_savehist
   fi
   export bash_cursor_pos_file=$(mktemp -t bash.curs.XXXXXX)
   export bash_readline_cmd_file=$(mktemp -t bash.rdln.XXXXXX)
@@ -13,7 +29,6 @@ re_search() {
   stty susp undef # allow us to use ctrl-z in re-search
   RE_SEARCH_SHELL=BASH re-search "$tmp"
   res="$?"
-  echo "$res"
   stty -ixoff
   [ -s "$tmp" ] || return 1
   READLINE_LINE="$(cat "$tmp")"
@@ -21,6 +36,11 @@ re_search() {
   rm -f "$tmp"
   return $res
 }
-# Beware! This requires xdotool to work. And this will only work under X.
+## Beware! This requires xdotool to work. And this will only work under X.
 bind -x '"\C-r":"if re_search; then xdotool key KP_Enter; fi"'
+## Here we could place the cursor to the position specified in $bash_cursor_pos_file,
+## then execute the readline command in $bash_readline_cmd_file and append the character
+## specified in $bash_append_char_file. Afterwards we can delete these files.
+## But it seems Bash doesn’t support this use case…
+rm -f "$tmp" "$bash_cursor_pos_file" "$bash_readline_cmd_file" "$bash_append_char_file"
 
--- bash_functions/re_search_delhist.sh
+++
@@ -1,4 +0,0 @@
-delhist() {
-  export bash_history_file="/tmp/re-search_bash_history_${USER}_${BASHPID}"
-  trap "rm -rf ${bash_history_file}" EXIT
-}
