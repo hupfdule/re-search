fef27a24d8d089e5299974778406a331cf748346
Marco Herrn <marco@mherrn.de>
2025-04-06 20:38:37 +0200

    Package re-search into a debian package
    This is intended to be used in a CI file, but can of course also be
    triggered manually.

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ DIST_DIR = dist
 SOURCES  = src/re-search.c
 HEADERS  = src/config.h src/version.h
 
-.PHONY: all debug clean package
+.PHONY: all debug clean package deb
 
 all: $(BIN_DIR)/re-search $(BIN_DIR)/re-search-static
 
@@ -22,6 +22,19 @@ debug: $(BIN_DIR)/re-search
 
 package: all
 	@sh package.sh
+
+deb: $(BIN_DIR)/re-search
+	@mkdir -p $(DIST_DIR)
+	@mkdir -p debian_pkg/DEBIAN
+	@mkdir -p debian_pkg/usr/bin
+	install -m 755 $(BIN_DIR)/re-search debian_pkg/usr/bin/
+	@echo "Version: $(shell grep 'define VERSION' src/version.h | cut -d '"' -f 2)" >> debian_pkg/DEBIAN/control
+	@echo "Architecture: $(shell dpkg --print-architecture)" >> debian_pkg/DEBIAN/control
+	@echo "Maintainer: $(DEB_MAINTAINER)" >> debian_pkg/DEBIAN/control
+	@echo "Description: Regular expression search tool" >> debian_pkg/DEBIAN/control
+	dpkg-deb --build debian_pkg $(DIST_DIR)/re-search_$(shell grep 'define VERSION' src/version.h | cut -d '"' -f 2)_$(shell dpkg --print-architecture).deb
+	@rm -rf debian_pkg
+
 
 clean:
 	rm -rf $(BIN_DIR) $(DIST_DIR)
--- package.sh
+++ package.sh
@@ -7,14 +7,19 @@ if [ -z "$VERSION" ]; then
     VERSION=$(date +%Y%m%d)
 fi
 
+ARCH=$(dpkg --print-architecture)
+
 # Directories
 BIN_DIR="bin"
 DIST_DIR="dist"
 FISH_FUNC_DIR="fish_functions"
+FISH_CONF_DIR="fish_config"
 TMP_DIR=$(mktemp -d)
+DEB_DIR=$(mktemp -d)
 
 # Cleanup trap
 trap 'rm -rf "$TMP_DIR"' EXIT
+trap 'rm -rf "$DEB_DIR"' EXIT
 
 # Package function for POSIX sh
 package_binary() {
@@ -42,6 +47,38 @@ package_binary() {
     echo "Created: $output_file"
 }
 
+# Package function for POSIX sh
+package_deb() {
+    binary_name=$1
+    output_file="${binary_name}_${VERSION}_${ARCH}.deb"
+
+    echo "Creating debian package…"
+
+    mkdir -p "${DEB_DIR}/DEBIAN"
+    mkdir -p "${DEB_DIR}/usr/bin"
+    mkdir -p "${DEB_DIR}/usr/share/fish/vendor_functions.d"
+    mkdir -p "${DEB_DIR}/usr/share/fish/vendor_conf.d"
+    mkdir -p "${DEB_DIR}/usr/share/doc/re-search"
+
+    cp "debian/control"            "${DEB_DIR}/DEBIAN"
+    cp "${BIN_DIR}/${binary_name}" "${DEB_DIR}/usr/bin/re-search"
+    cp "README.md"                 "${DEB_DIR}/usr/share/doc/re-search/"
+    cp "LICENSE"                   "${DEB_DIR}/usr/share/doc/re-search/"
+
+    cp -R "${FISH_FUNC_DIR}/"* "${DEB_DIR}/usr/share/fish/vendor_functions.d/"
+    cp -R "${FISH_CONF_DIR}/"* "${DEB_DIR}/usr/share/fish/vendor_conf.d/"
+
+    sed -i "s/^Version: .*$/Version: ${VERSION}/" "${DEB_DIR}/DEBIAN/control"
+    sed -i "s/^Architecture: .*$/Architecture: ${ARCH}/" "${DEB_DIR}/DEBIAN/control"
+
+    mkdir -p "${DIST_DIR}"
+    dpkg-deb --build ${DEB_DIR} "${DIST_DIR}/$output_file"
+
+    echo "Created: $output_file"
+}
+
 # Package both versions
 package_binary "re-search"
 package_binary "re-search-static"
+# Package only the dynamically linked one into a deb package
+package_deb    "re-search"
