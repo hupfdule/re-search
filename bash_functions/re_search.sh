7603db1363ad3193a903e78a7424bd9f24e462c3
Marco Herrn <marco@mherrn.de>
2025-04-06 22:09:10 +0200

    Fix history file var for bash

--- bash_functions/re_search.sh
+++ bash_functions/re_search.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 re_search_savehist() {
-  bash_pid_history_file=/tmp/my_bash_history_$$
+  bash_history_file="/tmp/re-search_bash_history_${USER}_${BASHPID}"
   ## Delete the history file on shell exit
   trap "rm -rf ${bash_history_file}" EXIT
 
   ## Save the whole history to our local history file if not yet done
-  if [ ! -f "$bash_pid_history_file" ]; then
-    history | sed 's/^[[:space:]]*[0-9]\+[[:space:]]\+\(.*\)/\1/g' > $bash_pid_history_file
+  if [ ! -f "$bash_history_file" ]; then
+    history | sed 's/^[[:space:]]*[0-9]\+[[:space:]]\+\(.*\)/\1/g' > $bash_history_file
   fi
 
   ## Append each executed command to the local history file
-  echo $(history 1 | sed "s/^[[:space:]]*[0-9]\+[[:space:]]\+\(.*\)/\1/g") >> $bash_pid_history_file
+  echo $(history 1 | sed "s/^[[:space:]]*[0-9]\+[[:space:]]\+\(.*\)/\1/g") >> $bash_history_file
 }
 ## save history every time a prompt is renewed (== after each command execution)
 export PROMPT_COMMAND='if [ "$(id -u)" -ne 0 ]; then re_search_savehist; fi'
